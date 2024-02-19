8e85e7fb9289f2cf622ebce430972276f782f328
idelton <94796876+idelton@users.noreply.github.com>
2024-02-19 03:34:55 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+
+// mobile Menu
+  const mobileMenu = document.getElementById('mobile-menu');
+
+  function menuButton() {
+    // Bascule la visibilité du menu au clic
+    mobileMenu.classList.toggle('hidden');
+  }
